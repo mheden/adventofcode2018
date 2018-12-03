9754bac18c804833d3be66a14ac2074e67ae94b8
Mikael Heden <mikael@heden.net>
2018-12-03 16:59:29 +0100

    Day 02

---
+++ 02.py
@@ -0,0 +1,42 @@
+def read_file(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+print("#--- part1 ---#")
+twos = 0
+threes = 0
+for id_ in read_file('02.txt'):
+    result = {}
+    for letter in list(id_):
+        result[letter] = result.get(letter, 0) + 1
+    s = set()
+    for k, v in result.items():
+        s.add(v)
+    if 2 in s:
+        twos += 1
+    if 3 in s:
+        threes += 1
+print(twos * threes)
+
+
+print("#--- part2 ---#")
+
+
+def compare(str1, str2):
+    diffs = 0
+    common = ''
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            diffs += 1
+        else:
+            common += str1[i]
+    return diffs, common
+
+ids = read_file('02.txt')
+for a in range(len(ids)):
+    for b in range(a + 1, len(ids)):
+        diffs, common = compare(ids[a], ids[b])
+        if diffs == 1:
+            print(common)
+            quit()
